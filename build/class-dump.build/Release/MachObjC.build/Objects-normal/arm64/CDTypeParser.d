be48b68756cd45edb6705b6105ed5e00c1e34b18
denglibing5 <denglibing3@jd.com>
2025-08-24 15:37:59 +0800

    fix: 修复编译粗我

---
+++ build/class-dump.build/Release/MachObjC.build/Objects-normal/arm64/CDTypeParser.d
@@ -0,0 +1,16 @@
+dependencies: \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/MachObjC-Prefix.pch \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDExtensions.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDTopoSortNode.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDTopologicalSortProtocol.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/NSArray-CDExtensions.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/NSData-CDExtensions.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/NSError-CDExtensions.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/NSScanner-CDExtensions.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/NSString-CDExtensions.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDTypeParser.m \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDTypeParser.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDMethodType.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDType.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDTypeName.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDTypeLexer.h
---
+++ build/class-dump.build/Release/MachObjC.build/Objects-normal/arm64/CDVisitorPropertyState.d
@@ -0,0 +1,13 @@
+dependencies: \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/MachObjC-Prefix.pch \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDExtensions.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDTopoSortNode.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDTopologicalSortProtocol.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/NSArray-CDExtensions.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/NSData-CDExtensions.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/NSError-CDExtensions.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/NSScanner-CDExtensions.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/NSString-CDExtensions.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDVisitorPropertyState.m \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDVisitorPropertyState.h \
+  /Users/denglibing/HDSpaces/HDPrivates/HDDream/app2dylib/class-dump/Source/CDOCProperty.h
